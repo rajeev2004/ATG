92e32ee2c959a16872aa8e281e47d8589d120c65
rajeev <rajeevchoudhary2425@gmail.com>
2025-01-11 04:20:15 +0530

    updated the files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ATG'
+  base:'/ATG',
+  build: {
+    outDir: 'dist', // Output folder for build
+  },
 })
