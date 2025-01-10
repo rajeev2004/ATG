217ea5bb32710951a922c29ba066407cf8e84425
rajeev <rajeevchoudhary2425@gmail.com>
2025-01-11 04:36:50 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import {HashRouter as Router,Routes,Route} from 'react-router-dom';
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import Register from "./components/Register";
